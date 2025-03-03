e8a6903c9036ecb617394b8af0c733125d95bfee
M-Abdullah-lab <m.abdullah06272@gmail.com>
2025-03-03 13:17:15 +0500

    Add Code to GitHub

---
+++ HomeTask3/HomeTask3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "The Sealed Nector";
+            book1.Author = "Safiur Rahman Mubarakpuri";
+            book1.ISBN = "9786035001106";
+            book1.Price = 4500;
+
+            book1.DisplayDatail();
+            book1.ApplyDiscount(10);
+        }
+    }
+}
